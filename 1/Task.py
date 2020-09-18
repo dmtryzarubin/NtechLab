5588acd9c1f4122f5adcbe7d30d45a70076619cc
Dmitriy Zarubin <dmtryzarubin@mail.ru>
2020-09-18 21:19:31 +0300

    Readme added

--- 1/Task.py
+++ 1/Task.py
@@ -1,21 +1,25 @@
+# Скрипт для нахождения подмассива с наибольшей сумме в массиве.
+# ввод массива
 a = input('Введите массив чисел через запятую, например : 56,65,-2,0 \nМассив: ')
 a = list(a.split(','))
 a = [int(i) for i in a]
 
-def ﬁndMaxSubArray(nums):
-    max_so_far = nums[0]
-    max_ending_here = nums[0]
+# Реализован алгоритм Кадана
+def ﬁndMaxSubArray(A):
+    max_so_far = A[0] 
+    max_ending_here = A[0] 
     start, stop = 0, 0
     
-    for i in range(1, len(nums)):
-        if (max_ending_here + nums[i]) > nums[i]:
-            max_ending_here = max_ending_here + nums[i]
-        elif max_ending_here + nums[i] <= nums[i]:
-            max_ending_here  = nums[i]
+    for i in range(1, len(A)):
+        if (max_ending_here + A[i]) > A[i]:
+            max_ending_here = max_ending_here + A[i]
+        elif max_ending_here + A[i] <= A[i]:
+            max_ending_here  = A[i]
             start = i
         if max_so_far <= max_ending_here:
             max_so_far = max_ending_here
             stop = i
-    return nums[start:stop+1]
+
+    return A[start:stop+1]
 
 print (ﬁndMaxSubArray(a))
